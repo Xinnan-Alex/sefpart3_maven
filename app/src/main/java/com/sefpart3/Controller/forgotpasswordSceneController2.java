41bd92e735cc561132b1d4c5e104f54e46a2606b
xinnan01991 <91050730+xinnan01991@users.noreply.github.com>
2021-11-21 00:53:42 +0800

    forgotpassword implemented

---
+++ app/src/main/java/com/sefpart3/Controller/forgotpasswordSceneController2.java
@@ -0,0 +1,51 @@
+package com.sefpart3.Controller;
+
+import java.io.IOException;
+
+import com.sefpart3.Model.Session;
+import com.sefpart3.Model.User;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+public class forgotpasswordSceneController2 {
+
+    @FXML
+    TextField oldpasswordField,newpasswordField;
+
+    @FXML
+    Button changepasswordButton,backButton;
+
+    private User user = Session.getInstance().getUser();
+
+    public void changepasswordButtonHandler() throws IOException{
+
+        if(user.getPassword().equals(oldpasswordField.getText())){
+            changepasword(newpasswordField.getText());
+            backButtonHandler();
+        }else{
+            new Alert(AlertType.ERROR,"Invalid old password, please try again!").showAndWait();
+        }
+    }
+
+    public void changepasword(String password){
+        user.setPassword(password);
+        user.write(false);
+    }
+
+    public void backButtonHandler() throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/loginScene.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
+    
+}
